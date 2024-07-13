e440f06342f5ee11adfe9fec318f9a3e1f9f5e86
lalit kaurav <lalitkaurav14@gmail.com>
2024-07-13 09:50:47 +0530

    Initial commit

---
+++ src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+             Imperial Grand Resort | All Right Reserved &copy; {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
